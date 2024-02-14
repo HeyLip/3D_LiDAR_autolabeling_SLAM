0d948652c8b1c4c295c0fd58aa557e6f9de4b33f
Sungjun Park <102511107+jagbum@users.noreply.github.com>
2024-02-14 13:50:06 +0900

    Delete deep_sdf directory

--- deep_sdf/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# Copyright 2004-present Facebook. All Rights Reserved.
-
-from deep_sdf import *
-from deep_sdf import *
-from deep_sdf import *
-from deep_sdf import *
-from deep_sdf import *
